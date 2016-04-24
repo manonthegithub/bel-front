34e7b1364f3c28036132844f29a555e4d67da7b1
Kirill <myworkpostbox@gmail.com>
2016-04-25 02:29:25 +0300

    testingo!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+var imag = require('./Nikon-D810-Image-Sample-7.jpg');
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <h1>Hello IRINA!.
+      	<img src={imag} alt="Smiley face"></img>
+      </h1>
     );
   }
 }
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,21 +1,15 @@
 var path = require('path');
-var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    filename: 'bundle.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, '/src/index.tmpl.html')
     })
@@ -29,7 +23,7 @@ module.exports = {
     },
     {
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }]
   }
